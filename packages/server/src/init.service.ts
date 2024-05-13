eac975ed3e1e1a8c63f0ee6e7c844685acb994e5
qq815776412 <815776412@qq.com>
2024-05-13 21:31:50 +0800

    fix: update refresh token timestamp from 48h to 4h feat: get default cloudUrl from progress.env; remove init get terminus version from cache;

--- packages/server/src/init.service.ts
+++ packages/server/src/init.service.ts
@@ -3,6 +3,7 @@ import { SecretService } from './secret.service';
 import axios from 'axios';
 import * as qs from 'qs';
 import { Interval } from '@nestjs/schedule';
+import { Cloud_URL } from './utils';
 
 export interface OSVersion {
   current_version: string;
@@ -14,8 +15,10 @@ export interface OSVersion {
 export class InitService implements OnModuleInit {
   private readonly logger = new Logger(InitService.name);
 
-  devVersion: OSVersion | null = null;
-  noDevVersion: OSVersion | null = null;
+  //   devVersion: OSVersion | null = null;
+  //   noDevVersion: OSVersion | null = null;
+
+  private cloudUrl = process.env.APP_SERVICE_CLOUD_URL || Cloud_URL;
 
   constructor(private secretService: SecretService) {
     //
@@ -26,27 +29,27 @@ export class InitService implements OnModuleInit {
     await this.getOSVersion(false);
   }
 
-  async updateLocalVersion() {
-    try {
-      const terminusInfo = await this.secretService.updateTerminusInfo();
-      if (this.devVersion) {
-        this.devVersion.current_version = terminusInfo.osVersion;
-      }
-      if (this.noDevVersion) {
-        this.noDevVersion.current_version = terminusInfo.osVersion;
-      }
-      return true;
-    } catch (e) {
-      console.log(e);
-      return false;
-    }
-  }
+  //   async updateLocalVersion() {
+  //     try {
+  //       const terminusInfo = await this.secretService.updateTerminusInfo();
+  //       if (this.devVersion) {
+  //         this.devVersion.current_version = terminusInfo.osVersion;
+  //       }
+  //       if (this.noDevVersion) {
+  //         this.noDevVersion.current_version = terminusInfo.osVersion;
+  //       }
+  //       return true;
+  //     } catch (e) {
+  //       console.log(e);
+  //       return false;
+  //     }
+  //   }
 
   async getOSVersion(dev_mode: boolean): Promise<OSVersion | null> {
     const terminusInfo = await this.secretService.updateTerminusInfo();
     try {
       const response: any = await axios.post(
-        'https://cloud-api.bttcdn.com/v1/resource/lastVersions',
+        this.cloudUrl + '/v1/resource/lastVersions',
         qs.stringify({
           versions: terminusInfo.osVersion,
           devMode: dev_mode,
@@ -61,21 +64,21 @@ export class InitService implements OnModuleInit {
         new_version: response.data.data.tag,
         is_new: response.data.data.tag != terminusInfo.osVersion,
       };
-      if (dev_mode) {
-        this.devVersion = ob;
-      } else {
-        this.noDevVersion = ob;
-      }
+      //   if (dev_mode) {
+      //     this.devVersion = ob;
+      //   } else {
+      //     this.noDevVersion = ob;
+      //   }
       return ob;
     } catch (e) {
       return null;
     }
   }
 
-  @Interval(1000 * 3600)
-  async refreshOSVerions() {
-    this.logger.log('refreshOSVerions');
-    await this.getOSVersion(true);
-    await this.getOSVersion(false);
-  }
+  //   @Interval(1000 * 3600)
+  //   async refreshOSVerions() {
+  //     this.logger.log('refreshOSVerions');
+  //     await this.getOSVersion(true);
+  //     await this.getOSVersion(false);
+  //   }
 }
