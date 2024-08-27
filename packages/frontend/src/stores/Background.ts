ae61401ccd8443cac1ed8a6e4f7f552e94caf6aa
qq815776412 <815776412@qq.com>
2024-08-27 21:52:25 +0800

    fix: background store use language error

--- packages/frontend/src/stores/Background.ts
+++ packages/frontend/src/stores/Background.ts
@@ -6,7 +6,6 @@ import { getSecondLevelDomain } from '../utils/constants';
 import { ThemeDefinedMode, themeModeName } from '@bytetrade/ui';
 import { i18n } from '../boot/i18n';
 import { useDeviceStore } from './device';
-import { SupportLanguageType } from 'src/i18n';
 
 export interface Wallpaper {
 	desktop: string;
@@ -34,7 +33,6 @@ export type BackgroundState = {
 	//
 	wallpaper: Wallpaper;
 	theme: ThemeDefinedMode;
-	locale: SupportLanguageType;
 	isMobile: boolean;
 };
 
@@ -172,13 +170,6 @@ export const useBackgroundStore = defineStore('background', {
 				Dark.set('auto');
 			} else {
 				Dark.set(this.theme == ThemeDefinedMode.DARK);
-			}
-		},
-		async updateLanguageLocale(locale: SupportLanguageType) {
-			this.locale = locale;
-			// await userModeSetItem('locale', this.locale);
-			if (this.locale) {
-				i18n.global.locale.value = this.locale;
 			}
 		}
 	}
