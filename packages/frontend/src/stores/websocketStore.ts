a6160d14a55b7d9dda876171e9fef9914405b715
qq815776412 <815776412@qq.com>
2025-03-13 11:44:06 +0800

    feat: update settings websocket

--- packages/frontend/src/stores/websocketStore.ts
+++ packages/frontend/src/stores/websocketStore.ts
@@ -33,82 +33,109 @@ export const useSocketStore = defineStore('counter', {
 
 	actions: {
 		start() {
-			let ws_url = process.env.WS_URL || window.location.origin + '/ws';
-
-			if (ws_url.startsWith('http://')) {
-				ws_url = ws_url.substring(7);
-				ws_url = 'ws://' + ws_url;
-			} else if (ws_url.startsWith('https://')) {
-				ws_url = ws_url.substring(8);
-				ws_url = 'wss://' + ws_url;
+			if (this.isConnecting() || this.isConnected()) {
+				console.log(
+					'socket Starting..., socket status' + this.websocket?.status
+				);
+				return;
 			}
 
-			this.websocket = new WebSocketBean({
-				url: ws_url,
-				needReconnect: true,
-				reconnectMaxNum: 5,
-				reconnectGapTime: 3000,
-				heartSend: JSON.stringify({
-					event: 'ping',
-					data: {}
-				}),
-				onopen: async () => {
-					this.send({
-						event: 'login',
-						data: { id: loginID }
-					});
-				},
-				onmessage: (ev) => {
-					try {
-						const message = JSON.parse(ev.data);
-						console.log('WebSocketBean onmessage', message);
+			if (!this.websocket) {
+				let ws_url =
+					process.env.WS_URL || window.location.origin + '/ws';
 
-						if (message.topic == MessageTopic.Data) {
-							// if (message.event == 'updateConfig') {
-							// 	const tokenStore = useTokenStore();
-							// 	tokenStore.config = message.message.data;
-							// }
-						} else {
-							if (message.event == 'app_installation_event') {
-								// bus.emit('app_installation_event', message);
-								const applicationStore = useApplicationStore();
-								applicationStore.get_applications();
-							} else if (
-								message.event == 'system_upgrade_event'
-							) {
-								bus.emit('system_upgrade_event', message.data);
-							} else if (message.event == 'ai') {
-								bus.emit('ai', message);
-							} else if (message.event == 'ai_message') {
-								bus.emit('ai_message', message);
-							} else if (message.event == 'intent') {
-								bus.emit('intent', message.data);
-							} else if (message.event == 'n') {
-								bus.emit('notification', message.data);
-							} else if (
-								message.event == 'entrance_state_event'
-							) {
-								const applicationStore = useApplicationStore();
-								applicationStore.get_applications().then(() => {
-									bus.emit('entrance_state_event', message);
-								});
-							}
-						}
-					} catch (e) {
-						console.log('message error');
-						console.log(e);
-					}
-				},
-				onerror: () => {
-					console.log('socket error');
-				},
-				onreconnect: () => {
-					console.log('socket start reconnect');
-				},
-				onFailReconnect: () => {
-					console.log('socket fail reconnect');
+				if (ws_url.startsWith('http://')) {
+					ws_url = ws_url.substring(7);
+					ws_url = 'ws://' + ws_url;
+				} else if (ws_url.startsWith('https://')) {
+					ws_url = ws_url.substring(8);
+					ws_url = 'wss://' + ws_url;
 				}
-			});
+
+				this.websocket = new WebSocketBean({
+					url: ws_url,
+					needReconnect: true,
+					heartSend: JSON.stringify({
+						event: 'ping'
+					}),
+					heartGet: JSON.stringify({
+						event: 'pong'
+					}),
+					onopen: async () => {
+						console.log('socket open ===>');
+						this.send({
+							event: 'login',
+							data: { id: loginID }
+						});
+					},
+					onmessage: async (ev) => {
+						try {
+							const message = JSON.parse(ev.data);
+							console.log('WebSocketBean onmessage', message);
+
+							if (message.topic == MessageTopic.Data) {
+								// if (message.event == 'updateConfig') {
+								// 	const tokenStore = useTokenStore();
+								// 	tokenStore.config = message.message.data;
+								// }
+							} else {
+								if (message.event == 'app_installation_event') {
+									// bus.emit('app_installation_event', message);
+									const applicationStore =
+										useApplicationStore();
+									applicationStore.get_applications();
+								} else if (
+									message.event == 'system_upgrade_event'
+								) {
+									bus.emit(
+										'system_upgrade_event',
+										message.data
+									);
+								} else if (message.event == 'ai') {
+									bus.emit('ai', message);
+								} else if (message.event == 'ai_message') {
+									bus.emit('ai_message', message);
+								} else if (message.event == 'intent') {
+									bus.emit('intent', message.data);
+								} else if (message.event == 'n') {
+									bus.emit('notification', message.data);
+								} else if (
+									message.event == 'entrance_state_event'
+								) {
+									const applicationStore =
+										useApplicationStore();
+									applicationStore
+										.get_applications()
+										.then(() => {
+											bus.emit(
+												'entrance_state_event',
+												message
+											);
+										});
+								}
+							}
+						} catch (e) {
+							console.log('message error');
+							console.log(e);
+						}
+					},
+					onerror: () => {
+						console.log('socket error');
+					},
+					onreconnect: async () => {
+						console.log('socket reconnecting');
+					},
+					onReconnectSuccess: async () => {
+						const applicationStore = useApplicationStore();
+						applicationStore.get_applications().then(() => {
+							bus.emit('entrance_state_event');
+						});
+					},
+					onReconnectFailure: () => {
+						console.log('socket fail reconnect');
+					}
+				});
+			}
 			this.websocket.start();
 			console.log('socket start !!!!');
 		},
@@ -118,6 +145,13 @@ export const useSocketStore = defineStore('counter', {
 				return false;
 			}
 			return this.websocket.status == WebSocketStatusEnum.open;
+		},
+
+		isConnecting() {
+			if (!this.websocket) {
+				return false;
+			}
+			return this.websocket.status == WebSocketStatusEnum.load;
 		},
 		send(data: any, resend = false) {
 			if (!this.websocket) {
